9754c8fccccc6a4fcc450a82cd5b4e29e9b8a503
sahil-jassal-sf <sahil.jassal@sourcefuse.com>
2024-01-03 15:31:09 +0530

    fix chat datasource binding key

--- services/chat-service/src/datasources/chat.datasource.ts
+++ services/chat-service/src/datasources/chat.datasource.ts
@@ -11,9 +11,8 @@ const config = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-      schema: process.env.DB_SCHEMA,
-    password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 };
 
 // Observe application's life cycle to disconnect the datasource when
@@ -21,11 +20,11 @@ const config = {
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
-export class ChatDataSource extends juggler.DataSource
-  implements LifeCycleObserver { 
-  
-    static dataSourceName = 'chat';
-  	
+export class ChatDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
+  static dataSourceName = 'chatDb';
 
   static readonly defaultConfig = config;
 
